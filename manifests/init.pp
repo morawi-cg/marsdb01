f7dc7ec47fd4ba39c0240fe19dedff42af5907eb
Mo Rawi <mo.rawi@reedbusiness.com>
2019-02-19 11:41:08 +0000

    this is the call to subclasses

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,12 @@
 class marsdb01 {
 
-    include setupmarsdb01repo
-    include installmarsdb01
-    include servicemarsdb01
+    #include marsdb01::setupmarsdb01repo
+    #include marsdb01::installmarsdb01
+    #include marsdb01::servicemarsdb01
+   
+    #Class['marsdb
 
+    class {marsdb01::setupmarsdb01repo:}
+    class {marsdb01::installmarsdb01:}
+    class {marsdb01::servicemarsdb01:}
 }
